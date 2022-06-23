685cfc8528dd2b80c0958f3950699ed09826827f
Temi <temiapantaku@gmail.com>
2022-06-23 20:20:31 +0100

    remove % operator option

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var firstNum = prompt("Please enter the first number: ");
-var operator = prompt("Enter the operator:\n (+, -, /, *, %)");
+var operator = prompt("Enter the operator:\n (+, -, /, *)");
 var secondNum = prompt("Please enter the second number: ");
 
 if (operator === "+") {
@@ -21,9 +21,4 @@ else if (operator === "*") {
     var solution = Number(firstNum) * Number(secondNum);
     solution = solution.toFixed(3);
     alert(firstNum + "*" + secondNum + "=" + solution);
-}
-else if (operator === "%") {
-    var solution = Number(firstNum) % Number(secondNum);
-    solution = solution.toFixed(3);
-    alert(firstNum + "%" + secondNum + "=" + solution);
 }
